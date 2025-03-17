2c02d89a974558381fa2586af6fb8f36d10b7a49
Vincent Barrier <vbarrier@kagilum.com>
2025-03-17 23:43:24 +0100

    Update configure-meshcentral.sh

--- setup/pi/configure-meshcentral.sh
+++ setup/pi/configure-meshcentral.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grep -qxF 'export HOME=/root' foo.bar || echo 'export HOME=/root' >> ~/.bashrc
+grep -qxF 'export HOME=/root' ~/.bashrc || echo 'export HOME=/root' >> ~/.bashrc
 source ~/.bashrc
 
 cd $HOME
