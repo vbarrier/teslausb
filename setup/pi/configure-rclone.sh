d0fead98c8acd650b605a525459ae474faf65658
Vincent Barrier <vbarrier@kagilum.com>
2025-03-17 23:59:41 +0100

    Update configure-rclone.sh

--- setup/pi/configure-rclone.sh
+++ setup/pi/configure-rclone.sh
@@ -3,6 +3,7 @@
 curl https://rclone.org/install.sh | sudo bash
 
 mkdir -p $HOME/.config/rclone
+rm -f $HOME/.config/rclone/rclone.conf
 
 echo "create rclone config"
 cat <<EOF > $HOME/.config/rclone/rclone.conf
